f017f73006e41706a0fc9eb1a3e838372a07228d
Vinícius Garcia Leão <32495036+ViniGarcia10@users.noreply.github.com>
2022-12-02 12:47:36 -0400

    add image and stylesheet and resize finnaly landpage

---
+++ src/components/Banner.tsx
@@ -0,0 +1,13 @@
+export const Banner = () => {
+  return (
+    <div className="container max-w-5xl m-auto p-6 flex-1 flex flex-col justify-center items-center sm:items-start text-center sm:text-left">
+      <span className="text-gray">
+        Criamos a Vinixx Software para te ajudar
+      </span>
+      <div className="uppercase text-4xl font-extrabold leanding-snug">
+        Desenvolver <span className="text-green">Software</span> de Verdade
+      </div>
+      <span className="text-gray">e se tornar um dev de sucesso!</span>
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: "#c7d0d8",
+        jungle: "#1a1d28",
+        gunmetal: "#2e3142",
+        green: "#43bc70",
+        red: "#F25F5C",
+      },
+      textColor: {
+        primary: "#FFFFFF",
+        secondary: "#c7d0d8",
+      },
+      container: {
+        center: true,
+      },
+    },
   },
   plugins: [],
-}
+};
